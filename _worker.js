2c5b70c3b2d25df67ffc3b6df57d439b10d6088f
NZNL <cc1253214@gmail.com>
2026-07-14 13:03:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1971,6 +1971,7 @@ async function forwardataTCP(host, portNum, rawData, ws, respHeader, remoteConnW
 	const ctx反代IP = 反代上下文.反代IP || '';
 	const ctx代理类型 = 反代上下文.代理类型 !== undefined ? 反代上下文.代理类型 : null;
 	const ctx代理全局 = 反代上下文.代理全局 !== undefined ? 反代上下文.代理全局 : false;
+	log(`[DEBUG反代] IP=${ctx反代IP} GLOBAL=${ctx代理全局}`);
 	const ctx代理参数 = 反代上下文.代理参数 || {};
 	const ctx反代兜底 = 反代上下文.反代兜底 !== undefined ? 反代上下文.反代兜底 : true;
 	let 反代数组索引 = 0;
